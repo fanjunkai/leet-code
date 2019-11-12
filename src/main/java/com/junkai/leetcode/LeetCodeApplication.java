39ed0de8aac2e688537f560fe570d69a2f791f92
asdfasdf <c159c7dcd67941049d58aaa2494ee663@bae-172aa575.com>
2019-11-12 19:55:49 +0800

    #1 init leet code repository

---
+++ src/main/java/com/junkai/leetcode/LeetCodeApplication.java
@@ -0,0 +1,13 @@
+package com.junkai.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junkai/leetcode/LeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junkai.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
